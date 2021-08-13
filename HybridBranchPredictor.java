b4672837876e782386a6aca2bdef3b4bb83a10b0
parkertscott <parkertscott@gmail.com>
2021-08-13 14:18:49 -0400

    Test Commit 2

--- HybridBranchPredictor.java
+++ HybridBranchPredictor.java
@@ -1,6 +1,6 @@
 public class HybridBranchPredictor
 {
-    int NUM_CHOOSER_COUNTERSa;
+    int NUM_CHOOSER_COUNTERS;
     int[] chooserCounters;
     int numPredictions = 0;
     int numMispredictions = 0;
